2597c06f0651645e24adc462691f9bd58ee2a8ba
richardingwe <richardingwe5@gmail.com>
2021-06-20 01:33:12 +0100

    created an about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React from "react";
+import { Link } from 'gatsby';
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <Navbar />
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX designer & web developer based in Manchester.</p>
+        <Link to="/projects">My Portfolio Projects</Link>
+      </div>
+    </section>
+  );
 }
